125b7f9351a13909dcfc589c0c09fdae4b928591
Spiss, Lukas <lukas.spiss@scandio.de>
2019-03-31 13:21:38 +0200

    Update

--- Scripts/monitor.sh
+++ Scripts/monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#date >> /home/lukas/Tmp/udev.log
 export DISPLAY=:0
-export XAUTHORITY=/run/user/1000/gdm/Xauthority
-X_USER=$(w -h -s | grep ":[0-9]\W" | head -1 | awk '{print $1}')
+export XAUTHORITY=/home/lukas/.Xauthority
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+X_USER=lukas
 
 # Get out of town if something errors
 set -e
@@ -11,31 +11,29 @@ set -e
 DP2_STATUS=`cat /sys/class/drm/card0/card0-DP-2/status`
 DP1_STATUS=`cat /sys/class/drm/card0/card0-DP-1/status`
 
-/bin/su $X_USER -c 'gsettings set org.gnome.desktop.interface scaling-factor 1'
-
 if [ "connected" == "$DP2_STATUS" ]; then  
-    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP-1 --off'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-2 --auto'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-2 --auto'
-    /bin/su $X_USER -c '/usr/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.5'
-    /bin/su $X_USER -c 'killall polybar'
-    /bin/su $X_USER -c '/home/lukas/.config/polybar/polybar.sh hdpi_bar'
-    /bin/su $X_USER -c '/usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "DP-2 plugged in"'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP1 --off'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP2 --auto'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP2 --auto'
+    /bin/su $X_USER -c '/usr/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.4'
+    /bin/su $X_USER -c 'killall -q polybar'
+    /bin/su $X_USER -c 'polybar --config=/home/lukas/.config/polybar/config hdpi_bar'
+    /bin/su $X_USER -c '/usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "DP2 plugged in"'
 elif [ "connected" == "$DP1_STATUS" ]; then  
-    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP-1 --off'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-1 --auto'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-1 --auto'
-    /bin/su $X_USER -c '/usr/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.5'
-    /bin/su $X_USER -c 'killall polybar'
-    /bin/su $X_USER -c '/home/lukas/.config/polybar/polybar.sh hdpi_bar'
-    /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "DP-1 plugged in"
+    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP1 --off'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP1 --auto'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP1 --auto'
+    /bin/su $X_USER -c '/usr/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.4'
+    /bin/su $X_USER -c 'killall -q polybar'
+    /bin/su $X_USER -c 'polybar --config=/home/lukas/.config/polybar/config hdpi_bar'
+    /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "DP1 plugged in"
 else  
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-1 --off'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output DP-2 --off'
-    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP-1 --auto'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP1 --off'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output DP2 --off'
+    /bin/su $X_USER -c '/usr/bin/xrandr --output eDP1 --auto'
     /bin/su $X_USER -c '/usr/bin/gsettings set org.gnome.desktop.interface text-scaling-factor 1.0'
-    /bin/su $X_USER -c 'killall polybar'
-    /bin/su $X_USER -c '/home/lukas/.config/polybar/polybar.sh full-hd_bar'
+    /bin/su $X_USER -c 'killall -q polybar'
+    /bin/su $X_USER -c 'polybar --config=/home/lukas/.config/polybar/config full-hd_bar'
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "External monitor disconnected"    
     exit
 fi
